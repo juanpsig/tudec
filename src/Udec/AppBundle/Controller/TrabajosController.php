e4db2141810c0a8b7ff0a3eb410d145ac9445115
Walther Bojaca <waltherfbg@yahoo.com>
2016-05-30 19:02:37 -0500

    Se agrega para buscar trabajos y se inicia el formulario para crear trabajos

--- src/Udec/AppBundle/Controller/TrabajosController.php
+++ src/Udec/AppBundle/Controller/TrabajosController.php
@@ -22,6 +22,29 @@ class TrabajosController extends Controller {
     }
     
     public function buscarAction(){
-        return $this->render('UdecAppBundle:Trabajos:buscar.html.twig');
+        $rqt = $this->get("request");
+        if($rqt->get("acc")){
+            $text = str_replace(' ','%',$rqt->get('txtkey'));
+            $info = $this->queryTrabajos("WHERE tb.titulo like'%$text%'");
+            return $this->render('UdecAppBundle:Trabajos:listBuscar.html.twig',array('info'=>$info));
+        }else{
+            $info = $this->queryTrabajos('');
+            return $this->render('UdecAppBundle:Trabajos:buscar.html.twig',array('info'=>$info));
+        }
+    }
+    
+    public function queryTrabajos($where){
+        $em = $this->getDoctrine()->getManager();
+        $query = "SELECT tb.id idTrabajo,tb.fecha_rg fechaTrabajo,tb.titulo tituloTrabajo,tb.concepto conceptoTrabajo,tb.fecha_grado fechaGradoTRabajo
+                    ,GROUP_CONCAT(DISTINCT(ps.id)) idsPersonas,GROUP_CONCAT(DISTINCT(ps.numero_doc)) docsPersonas
+                    FROM autores au
+                    INNER JOIN trabgrado tb ON tb.id = au.id_trabajo
+                    INNER JOIN personas ps ON ps.id = au.id_persona
+                    $where
+                    GROUP BY au.id_trabajo
+                    ORDER BY tb.id DESC LIMIT 50";
+        $con = $em->getConnection()->executeQuery($query);
+        $con->execute();
+        return $con->fetchAll();
     }
 }
