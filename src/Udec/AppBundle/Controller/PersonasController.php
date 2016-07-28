f5bf431b255745f024aa3b2c91ec248bcce50d07
Walther Bojaca <waltherfbg@yahoo.com>
2016-07-27 19:48:02 -0500

    Se agregan las funciones de busqueda de personas y se aplica en agregar autores

--- src/Udec/AppBundle/Controller/PersonasController.php
+++ src/Udec/AppBundle/Controller/PersonasController.php
@@ -17,11 +17,32 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class PersonasController extends Controller {
     
+    public function getEm(){
+        return $this->getDoctrine()->getManager();
+    }
+
     public function nuevoAction(){
         return new Response('ok');
     }
     
     public function buscarAction(){
-        return new Response('ok');
+        $rqt = $this->get('request');
+        if($rqt->get('texto')){
+            $buscar = '+'.str_replace(' ','* +',$rqt->get('texto')).'*';
+            $personas = $this->queryPersonas("WHERE MATCH(primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,codigo,email,telefono_fijo,movil,numero_doc) AGAINST('$buscar' IN BOOLEAN MODE)");
+            return $this->render('UdecAppBundle:Personas:listaPersonas.html.twig',array('lista'=>$personas));
+        }
+        return new Response('',404);
+    }
+    
+    public function queryPersonas($where){
+        $em = $this->getEm();
+        $query = "SELECT id,CONCAT(primer_nombre,' ',segundo_nombre) nombre,CONCAT_WS(' ',primer_apellido,segundo_apellido) apellidos,codigo,email,telefono_fijo telefono,movil,tipo_doc tipoDoc,numero_doc numeroDoc,if(estado = '1','Activo','Inactivo') estado
+                    FROM personas
+                    $where";
+        //print_r($query);
+        $con = $em->getConnection()->prepare($query);
+        $con->execute();
+        return $con->fetchAll();
     }
 }
